d0c93205d496d3203ce4942e4e70c9cdecb37703
huytl2107 <huytl2107@gmail.com>
2023-12-04 20:41:26 +0700

    them ghi chu

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    //Nhập dữ liệu sản phẩm
     var products = [
         { image: 'https://a.cdn-hotels.com/gdcs/production0/d1513/35c1c89e-408c-4449-9abe-f109068f40c0.jpg?impolicy=fcrop&w=800&h=533&q=medium', title: 'Sản phẩm 1', description: 'Mô tả sản phẩm 1' },
         { image: 'https://www.thephotoargus.com/wp-content/uploads/2021/04/food-photography-tips-for-toast_AFTER-1024x682.jpeg', title: 'Sản phẩm 2', description: 'Mô tả sản phẩm 2' },
@@ -9,7 +10,7 @@ window.onload = function() {
         { image: 'https://a.cdn-hotels.com/gdcs/production0/d1513/35c1c89e-408c-4449-9abe-f109068f40c0.jpg?impolicy=fcrop&w=800&h=533&q=medium', title: 'Sản phẩm 3', description: 'Mô tả sản phẩm 3' },
         { image: 'https://www.thephotoargus.com/wp-content/uploads/2021/04/food-photography-tips-for-toast_AFTER-1024x682.jpeg', title: 'Sản phẩm 4', description: 'Mô tả sản phẩm 4' },
     ];
-
+    //Thêm sản phẩm vào index.html
     var productContainer = document.getElementById('sanpham');
     products.forEach(function(product) {
         var card = document.createElement('div');
