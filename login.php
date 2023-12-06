d982f365e7981aab8109f43d4776665ef3d8deea
huytl2107 <huytl2107@gmail.com>
2023-12-06 21:09:58 +0700

    đăng ký, insert user vào database

--- login.php
+++ login.php
@@ -3,11 +3,12 @@
     $sdt = $_POST['sdt'];
     $pass = $_POST['pass'];
     //kết nối đến cơ sở dữ liệu
-    $db = mysqli_connect("127.0.0.1", "root", "", "webbanhang");
+    require 'db/connect.php';
+    
     //truy vấn dữ liệu
     $sql = "select * from tbl_user where sdt='$sdt' and pass='$pass'";
 
-    $rs = mysqli_query($db, $sql);
+    $rs = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($rs) > 0){
         echo "<h2>Đăng nhập thành công</h2>";
---
+++ signup.php
@@ -0,0 +1,26 @@
+<?php
+//kết nối với database
+require 'db/connect.php';
+if(isset($_POST['btn-dangky'])){
+    $sdt_dangky = $_POST['sdt_dangky'];
+    $pass_dangky = $_POST['pass_dangky'];
+    $cfpass_dangky = $_POST['cfpass_dangky'];
+    //kiếm tra và insert dữ liệu vào database
+    if(!empty($sdt_dangky)&&!empty($pass_dangky)&&!empty($cfpass_dangky)){
+        if($pass_dangky==$cfpass_dangky){
+            $sql = "INSERT INTO `tbl_user` (`sdt`, `pass`) VALUE('$sdt_dangky', '$pass_dangky')";
+            if($conn->query($sql)===TRUE){
+                echo "insert thành công";
+            }else{
+                echo "Lỗi";
+            }
+        }
+        else{
+            echo "Mật khẩu xác nhận không đúng";
+        }
+    } else {
+        echo "Bạn cần nhập đầy đủ thông tin";
+    }
+
+}
+?>
