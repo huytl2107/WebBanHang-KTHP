d0485d25a3c603131be74832f30bed5b85b23888
huytl2107 <huytl2107@gmail.com>
2023-12-07 19:33:17 +0700

    update code

--- db/db_module.php
+++ db/db_module.php
@@ -34,30 +34,4 @@ function giaiPhongBoNho($link, $result)
     } catch (TypeError $e) {
     }
 }
-    function taoKetnoi(&$link)
-    {
-        $link = mysqli_connect(HOST, USER, PASSWORD, DB);
-        if (mysqli_connect_error()){
-            echo "Lỗi kết nối đến máy chủ: " .mysqli_connect_error();
-            exit();
-        }
-
-    }
-    function chayTruyVanTraVeDuLieu($link, $q)
-    {
-        $result = mysqli_query($link, $q);
-        return $result;
-    }
-    function chayTruyVanKhongTraVeDuLieu($link, $q)
-    {
-        $result = mysqli_query($link, $q);
-        return $result;
-    }
-    function giaiPhongBoNho($link, $result)
-    {
-        try {
-            mysqli_close($link);
-            mysqli_free_result($result);
-        } catch(TypeError $e) {}
-    }
 ?>
--- login.php
+++ login.php
@@ -5,13 +5,11 @@
     //kết nối đến cơ sở dữ liệu
     require 'db/db_module.php';
     // Tạo kết nối
-    $link = null;  // Khởi tạo giá trị mặc định
-    taoKetnoi($link);
 
     //truy vấn dữ liệu
     $sql = "select * from tbl_user where sdt='$sdt' and pass='$pass'";
 
-    $rs = chayTruyVanTraVeDuLieu($link, $sql);
+    $rs = chayTruyVanTraVeDL($conn, $sql);
     print_r ($rs);
 
     if(mysqli_num_rows($rs) > 0){
