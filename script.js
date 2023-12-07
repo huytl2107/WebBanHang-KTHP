b47eebe0c415202fb710f44700ff8d2487ff609a
huytl2107 <huytl2107@gmail.com>
2023-12-07 17:16:51 +0700

    add config.php db_module.php

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     //Nhập dữ liệu sản phẩm
     var products = [
-        { id: 1, image: 'https://a.cdn-hotels.com/gdcs/production0/d1513/35c1c89e-408c-4449-9abe-f109068f40c0.jpg?impolicy=fcrop&w=800&h=533&q=medium', title: 'Sản phẩm 1', description: 'Mô tả sản phẩm 1', price: 40000, tag: "" },
+        { id: 1, image: 'https://lzd-img-global.slatic.net/g/p/7fc562f294af0757642454970fd0ac03.jpg_720x720q80.jpg', title: 'Sản phẩm 1', description: 'Mô tả sản phẩm 1', price: 40000, tag: "" },
         { id: 2, image: 'https://www.thephotoargus.com/wp-content/uploads/2021/04/food-photography-tips-for-toast_AFTER-1024x682.jpeg', title: 'Sản phẩm 2', description: 'Mô tả sản phẩm 2', price: 20000, tag: "" },
         { id: 3, image: 'https://a.cdn-hotels.com/gdcs/production0/d1513/35c1c89e-408c-4449-9abe-f109068f40c0.jpg?impolicy=fcrop&w=800&h=533&q=medium', title: 'Sản phẩm 3', description: 'Mô tả sản phẩm 3', price: 40000, tag: "" },
         { id: 4, image: 'https://www.thephotoargus.com/wp-content/uploads/2021/04/food-photography-tips-for-toast_AFTER-1024x682.jpeg', title: 'Sản phẩm 4', description: 'Mô tả sản phẩm 4', price: 20000, tag: "" },
@@ -25,11 +25,10 @@ window.onload = function() {
         card.className = 'card col-lg-3 col-md-6';
         card.innerHTML = `
             <a href="product.html">
-                <img class="card-img" src="${product.image}" alt="Card image">
+                <img class="larger-card-img" src="${product.image}" alt="Card image">
             </a>
             <div class="card-body d-flex flex-column align-items-center">
                 <h4><a href="product.html" class="card-title">${product.title}</a></h4>
-                <p class="card-text">${product.description}</p>
                 <p class="card-text">${product.price}đ</p>
                 <div class="d-flex justify-content-around w-100">
                     <a href="#" class="btn-buy btn btn-success">Buy</a>
