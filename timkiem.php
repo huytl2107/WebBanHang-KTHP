f26e23ea0360429c4c2cbf8340a8dca9389f26f0
thanhtam160603 <100037276+thanhtam160603@users.noreply.github.com>
2023-12-07 23:42:04 +0700

    aa

---
+++ timkiem.php
@@ -0,0 +1,23 @@
+<?php
+$keyword = $_POST['keyword'];
+require 'db/connect.php';
+
+if(isset($_GET['keyword'])){
+    $result = chayTruyVanTraVeDL($conn, "SELECT * FROM tbl_products WHERE ten LIKE '%".$_GET['keyword']."%'");
+
+    if($result) {
+        while($row = mysqli_fetch_assoc($result)){
+            echo "<div class='sanpham col-sm-10'>
+                    <div>
+                        <h2>".$row['ten']."</h2>
+                    </div>
+                  </div>";
+        }
+    } else {
+        echo "<h1>Không có sản phẩm</h1>";
+    }
+} else {
+    echo "<h1>Không có sản phẩm</h1>";
+}
+
+?>
