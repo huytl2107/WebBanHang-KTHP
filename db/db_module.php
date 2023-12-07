991a3a405d9faf9697c81c8be6f84ddf9e4d5b73
31211027571 <binhbui.31211027571@st.ueh.edu.vn>
2023-12-07 17:49:49 +0700

    add menu

--- db/db_module.php
+++ db/db_module.php
@@ -1,31 +1,37 @@
 <?php
-    require_once "db/config.php";
+// Include file connect.php to use the existing connection
+include('config.php');
 
-    function taoKetnoi($link)
-    {
-        $link = mysqli_connect(HOST, USER, PASSWORD, DB);
-        if (mysqli_connect_error()){
-            echo "Lỗi kết nối đến máy chủ: " .mysqli_connect_error();
-            exit();
-        }
+function chayTruyVanTraVeDL($link, $q)
+{
+    $result = mysqli_query($link, $q);
 
+    // Check if the query was successful
+    if (!$result) {
+        die("Lỗi truy vấn: " . mysqli_error($link));
+    }
 
+    return $result;
+}
+
+function chayTruyVanKhongTraVeDL($link, $q)
+{
+    $result = mysqli_query($link, $q);
+
+    // Check if the query was successful
+    if (!$result) {
+        die("Lỗi truy vấn: " . mysqli_error($link));
     }
-    function chayTruyVanTraVeDuLieu($link, $q)
-    {
-        $result = mysqli_query($link, $q);
-        return $result;
+
+    return $result;
+}
+
+function giaiPhongBoNho($link, $result)
+{
+    try {
+        mysqli_close($link);
+        mysqli_free_result($result);
+    } catch (TypeError $e) {
     }
-    function chayTruyVanKhongTraVeDuLieu($link, $q)
-    {
-        $result = mysqli_query($link, $q);
-        return $result;
-    }
-    function giaiPhongBoNho($link, $result)
-    {
-        try {
-            mysqli_close($link);
-            mysqli_free_result($result);
-        } catch(TypeError $e) {}
-    }
+}
 ?>
---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php
+// Include file connection.php to use the functions
+include('db\db_module.php');
+
+$result = chayTruyVanTraVeDL($conn, "select * from tbl_products_types");
+while($rows = mysqli_fetch_assoc($result)){
+    echo "<a href='?dm=".$rows['id_type']."' class='btn btn-link'>".$rows['name_type']."</a>";
+}
+?>
