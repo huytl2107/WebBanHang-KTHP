dc5c63d06239099d538f644edc10f64e136713a5
31211027571 <binhbui.31211027571@st.ueh.edu.vn>
2023-12-08 16:44:20 +0700

    Hiển thị sản phẩm home bỏ js

--- index.php
+++ index.php
@@ -26,12 +26,10 @@ session_start();
 			<div class="sanpham col-sm-10">
 				<div class="row" id="sanpham">
 					<!--Thêm sản phẩm từ databese-->
+					<?php include("hienthisp.php")?>
 				</div>
 			</div>
-		  </div>
-	<script src="script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  
+		  </div>  
 	<script>
 		// Fetch and include login modal
 		fetch('login-modal.html')
