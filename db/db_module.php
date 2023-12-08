7857b00460bc27983f4c4cadd1d6d41d9c405202
thanhtam160603 <100037276+thanhtam160603@users.noreply.github.com>
2023-12-08 14:10:36 +0700

    sửa lại liên kết sql

--- db/db_module.php
+++ db/db_module.php
@@ -2,34 +2,34 @@
 // Include file connect.php to use the existing connection
 include('config.php');
 
-function chayTruyVanTraVeDL($link, $q)
+function chayTruyVanTraVeDL($conn, $q)
 {
-    $result = mysqli_query($link, $q);
+    $result = mysqli_query($conn, $q);
 
     // Check if the query was successful
     if (!$result) {
-        die("Lỗi truy vấn: " . mysqli_error($link));
+        die("Lỗi truy vấn: " . mysqli_error($conn));
     }
 
     return $result;
 }
 
-function chayTruyVanKhongTraVeDL($link, $q)
+function chayTruyVanKhongTraVeDL($conn, $q)
 {
-    $result = mysqli_query($link, $q);
+    $result = mysqli_query($conn, $q);
 
     // Check if the query was successful
     if (!$result) {
-        die("Lỗi truy vấn: " . mysqli_error($link));
+        die("Lỗi truy vấn: " . mysqli_error($conn));
     }
 
     return $result;
 }
 
-function giaiPhongBoNho($link, $result)
+function giaiPhongBoNho($conn, $result)
 {
     try {
-        mysqli_close($link);
+        mysqli_close($conn);
         mysqli_free_result($result);
     } catch (TypeError $e) {
     }
