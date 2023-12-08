ec89e3b2e80ae95430355de8038ede240948fb24
huytl2107 <huytl2107@gmail.com>
2023-12-08 23:17:42 +0700

    thêm footer

---
+++ footer.php
@@ -0,0 +1,114 @@
+<?php
+    echo '
+
+
+
+  <!-- Footer -->
+  <footer
+          class="text-center text-lg-start text-white"
+          style="background-color: #3e4551"
+          >
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Links -->
+      <section class="">
+        <!--Grid row-->
+        <div class="row">
+          <!--Grid column-->
+          <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+            <h5 class="text-uppercase">HEYYOU!® - POPULAR BRAND</h5>
+
+            <p>
+            Hệ thống của hàng
+            TP Hồ Chí Minh:
+            5B đường số 2, khu phố 2, phường Tam Bình, TP. Thủ Đức
+            182/13A Lê Văn Sỹ, Phường 10, quận Phú Nhuận.
+            TP Cần Thơ:
+            Ninh Kiều: 47 đường Nguyễn Việt Hồng, Phường An Phú, Ninh Kiều, Cần Thơ
+            </p>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-lg-4 col-md-3 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Về chúng tôi</h5>
+
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-white">Giới thiệu</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Chính sách giao hàng</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Chính sách đổi trả</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white">Điều khoản dịch vụ</a>
+              </li>
+            </ul>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-lg-4 col-md-3 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Liên hệ</h5>
+
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#!" class="text-white"><i class="bi bi-telephone"></i> Hotline: 0934713377</a>
+              </li>
+              <li>
+                <a href="#!" class="text-white"><i class="bi bi-envelope"></i> Email: nguoibanhang@gmail.con</a>
+              </li>
+             
+            </ul>
+          </div>
+          <!--Grid column-->
+
+        </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
+
+      <!-- Section: Social media -->
+      <section class="mb-4 text-center">
+        <!-- Facebook -->
+        <a
+           class="btn btn-outline-light btn-floating m-1"
+           href="#!"
+           role="button"
+           ><i class="bi bi-facebook"></i></a>
+
+        <!-- Twitter -->
+        <a
+           class="btn btn-outline-light btn-floating m-1"
+           href="#!"
+           role="button"
+           ><i class="bi bi-twitter-x"></i></a>
+
+        <!-- Google -->
+        <a
+           class="btn btn-outline-light btn-floating m-1"
+           href="#!"
+           role="button"
+           ><i class="bi bi-google"></i></a>
+
+        <!-- Instagram -->
+        <a
+           class="btn btn-outline-light btn-floating m-1"
+           href="#!"
+           role="button"
+           ><i class="bi bi-instagram"></i></a>
+
+      </section>
+      <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+
+  </footer>
+  <!-- Footer -->
+
+  
+    ';
+?>
